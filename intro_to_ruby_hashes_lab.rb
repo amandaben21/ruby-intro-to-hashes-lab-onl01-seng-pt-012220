a4384ba450ce449cbf3c04c704a4aaedb4020abc
Amanda Contreras <amandaben21@aol.com>
2020-02-03 03:55:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,6 +33,8 @@ def monopoly_with_third_tier
 	    :pieces => 4,
 	    :names => {},
 	    :rent_in_dollars => {}
+	    :pennsylvania_railroad => {}
+	    :b_and_o_railroad => {}
 	      :one_piece_owned => 25,
 	      :two_piece_owned => 50,
 	      :three_pieces_owned => 100,
